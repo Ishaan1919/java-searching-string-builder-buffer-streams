682a1e6eb97236dbca2a53c454eed98f0db98e65
Ishaan <ishaan0642.be21@chitkara.edu.in>
2025-03-25 23:07:28 +0530

    Program for counting occurence of a word in the file

---
+++ WordCount.java
@@ -0,0 +1,26 @@
+import java.io.*;
+
+public class WordCount {
+    public static void main(String[] args) {
+
+        String filename = "sample.txt";
+        String targetWord = "is";
+        int count = 0;
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] words = line.split("\\s+");
+                for (String word : words) {
+                    if (word.equalsIgnoreCase(targetWord)) {
+                        count++;
+                    }
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Error reading file: " + e.getMessage());
+        }
+
+        System.out.println("The word '" + targetWord + "' appears " + count + " times in the file.");
+    }
+}
